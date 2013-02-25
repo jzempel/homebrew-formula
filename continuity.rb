9a72ab7d55b1c2960ca236685dd0e41803e9418f
Jonathan Zempel <jzempel@gmail.com>
2013-02-24 20:08:49 -0800

    Switch from MD5 to SHA1

--- continuity.rb
+++ continuity.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Continuity < Formula
   homepage 'https://github.com/jzempel/continuity'
   url 'https://github.com/jzempel/continuity/tarball/0.4'
-  md5 'bb3103334f1e6f2ad35c5556b96a9968'
+  sha1 '1cb3f0c279e6c0158be3ad83d57b24a3d0b349bb'
   head 'https://github.com/jzempel/continuity.git'
 
   # The pyinstaller-built binary complains on strip.
