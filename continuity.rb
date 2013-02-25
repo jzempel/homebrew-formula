3bb4203bff6ac4e42165970b7719372c28754863
Jonathan Zempel <jzempel@gmail.com>
2013-02-25 08:43:52 -0800

    Update continuity SHA1

--- continuity.rb
+++ continuity.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Continuity < Formula
   homepage 'https://github.com/jzempel/continuity'
   url 'https://github.com/jzempel/continuity/tarball/0.4'
-  sha1 'c3282a8fbba6afd4346c2bd93987054933465d05'
+  sha1 '676b8eeee3bb0ae0044ba361e2baa141d3340bf6'
   head 'https://github.com/jzempel/continuity.git'
 
   # The pyinstaller-built binary complains on strip.
