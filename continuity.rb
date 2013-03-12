eac4ad769c031011c6abe514a997384969472586
Jonathan Zempel <jzempel@gmail.com>
2013-03-12 08:01:36 -0700

    Update to continuity 0.4.2

--- continuity.rb
+++ continuity.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Continuity < Formula
   homepage 'https://github.com/jzempel/continuity'
-  url 'https://github.com/jzempel/continuity/tarball/0.4'
-  sha1 '5b85b4f26831418d90b333a03c6d4c235168e35a'
+  url 'https://github.com/jzempel/continuity/tarball/0.4.2'
+  sha1 '88199d8f7c1d2b72d1f24d1249473a56aef631d7'
   head 'https://github.com/jzempel/continuity.git'
 
   # The pyinstaller-built binary complains on strip.
