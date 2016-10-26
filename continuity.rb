08797223fd7af0eae0fdec5e6038fc9ae0cfa3ee
Jonathan Zempel <jzempel@gmail.com>
2016-10-26 16:58:37 -0700

    Update continuity URL

--- continuity.rb
+++ continuity.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Continuity < Formula
   homepage 'https://github.com/jzempel/continuity'
-  url 'https://pypi.python.org/packages/source/c/continuity/continuity-0.10.3.tar.gz'
+  url 'https://pypi.python.org/packages/00/b8/75c565f94c04795089ff5debb94e3eb30b542309f0490eec4a104c46c27d/continuity-0.10.3.tar.gz'
   sha256 'fe6ed4b6499099cc7ae0a579704f0fee558ce3170bb18ee2ab1b37186d690103'
   head 'https://github.com/jzempel/continuity.git'
 
