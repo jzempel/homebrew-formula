0d69eb07f39e37a1024d4e783003946c2a36cfb7
Jonathan Zempel <jzempel@gmail.com>
2017-11-05 14:14:43 -0800

    Update to continuity 1.0.0

--- continuity.rb
+++ continuity.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Continuity < Formula
   homepage 'https://github.com/jzempel/continuity'
-  url 'https://pypi.python.org/packages/00/b8/75c565f94c04795089ff5debb94e3eb30b542309f0490eec4a104c46c27d/continuity-0.10.3.tar.gz'
-  sha256 'fe6ed4b6499099cc7ae0a579704f0fee558ce3170bb18ee2ab1b37186d690103'
+  url 'https://pypi.python.org/packages/aa/88/4248c2aae19d71876a30c759c43d855c1e47cc2e23791aa8fdb81bb8ecc5/continuity-1.0.0.tar.gz'
+  sha256 '901cb78e6c4bbe0745dd160313032cf5b74743b45e09849cc74c6d519944f2fe'
   head 'https://github.com/jzempel/continuity.git'
 
   # The pyinstaller-built binary complains on strip.
